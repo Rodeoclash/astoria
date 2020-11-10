9a2d8fa6ec986d07d3efbedeb57000ac03c06bdf
Sam Richardson <sam@richardson.co.nz>
2020-11-10 21:56:53 +1100

    Store rate limits in other interactions

--- server/lib/astoria/interactions/sync_github_installations.ex
+++ server/lib/astoria/interactions/sync_github_installations.ex
@@ -2,11 +2,12 @@ defmodule Astoria.Interactions.SyncGithubInstallations do
   alias Astoria.{Github, GithubInstallations}
 
   def perform(request) do
-    with {:ok, response} <- Github.Api.V3.Request.perform(request),
-         do:
-           Enum.map(response.data.body, fn installation ->
-             with {:ok, github_installation} <- GithubInstallations.upsert(installation),
-                  do: GithubInstallations.GithubRepositories.sync(github_installation)
-           end)
+    case Github.Api.V3.Request.perform(request) do
+      {:ok, response} ->
+        Enum.map(response.data.body, fn installation ->
+          with {:ok, github_installation} <- GithubInstallations.upsert(installation),
+               do: GithubInstallations.GithubRepositories.sync(github_installation)
+        end)
+    end
   end
 end
--- server/lib/astoria/interactions/sync_github_pull_request.ex
+++ server/lib/astoria/interactions/sync_github_pull_request.ex
@@ -1,10 +1,17 @@
 defmodule Astoria.Interactions.SyncGithubPullRequest do
-  alias Astoria.{Github, GithubRepositories, Repo}
+  alias Astoria.{Github, GithubRepositories, Repo, Interactions}
+
+  import Interactions.SyncGithub
 
   def perform(request, github_repository_id) do
-    github_repository = Repo.get(GithubRepositories.GithubRepository, github_repository_id)
+    github_repository =
+      Repo.get(GithubRepositories.GithubRepository, github_repository_id)
+      |> Repo.preload(:github_installation)
 
-    with {:ok, response} <- Github.Api.V3.Request.perform(request),
-         do: GithubRepositories.GithubPullRequests.upsert(github_repository, response.data.body)
+    case Github.Api.V3.Request.perform(request) do
+      {:ok, response} ->
+        update_github_installation_rate_limits(github_repository.github_installation, response.data)
+        GithubRepositories.GithubPullRequests.upsert(github_repository, response.data.body)
+    end
   end
 end
