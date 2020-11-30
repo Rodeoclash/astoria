85e28fac7531cb229cb193f034b8a3eef9789bca
Sam Richardson <sam@richardson.co.nz>
2020-11-30 20:35:21 +1100

    Cleanup chart frontend

---
+++ server/assets/src/dashboard/services/charts.js
@@ -0,0 +1,14 @@
+export const config = {};
+
+export const layout = {
+  autosize: true,
+  yaxis: {
+    rangemode: "tozero",
+    autorange: true,
+  },
+};
+
+export const style = {
+  height: "80%",
+  width: "100%",
+};
