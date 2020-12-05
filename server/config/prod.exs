a2341bcbaee796787b72318f737e446d2388dd54
Sam Richardson <sam@richardson.co.nz>
2020-12-05 20:23:10 +1100

    Enable https

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :astoria, AstoriaWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "astoria.stagehand.dev", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [hsts: true]
 
 # Do not print debug messages in production
 config :logger, level: :info
