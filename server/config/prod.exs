2b0afbbd709ac0a51ba328c15c220fd54478d7d2
Sam Richardson <sam@richardson.co.nz>
2020-12-05 20:35:00 +1100

    Remove for ssl

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :astoria, AstoriaWeb.Endpoint,
   url: [host: "astoria.stagehand.dev", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
