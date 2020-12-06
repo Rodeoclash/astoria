d46e27551df38a86ce2b03d1c3743ebbcf4a10a9
Sam Richardson <sam@richardson.co.nz>
2020-12-06 21:46:09 +1100

    Configure bucket

--- infra/terraform/config.tf
+++ infra/terraform/config.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
-    region = "us-east-1"
+    bucket = "astoria-terraform-state"
+    key    = "state.json"
+    region = "ap-southeast-2"
   }
 }
 
