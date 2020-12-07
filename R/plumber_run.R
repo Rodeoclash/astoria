fd3e54479c3daad2e93274e2a2789285f9c33046
Sam Richardson <sam@richardson.co.nz>
2020-12-07 22:55:52 +1100

    Missing library

--- R/plumber_run.R
+++
@@ -1,4 +0,0 @@
-library(plumber)
-# 'plumber.R' is the location of the file shown above
-pr("plumber.R") %>%
-  pr_run(port=8000)
--- analysis/plumber.R
+++ analysis/plumber.R
@@ -1,30 +1,69 @@
-#* Return the PRs merged for the month
-#* @param json input json payload
-#* @post /total_month
-function(json) {
-  payload <- jsonlite::fromJSON(json) %>% as.data.table()
-  payload$year <- lubridate::year(payload$merged_at)
-  payload$month <- lubridate::month(payload$merged_at)
-  payload$month_label <- lubridate::month(payload$merged_at, label = T)
-  payload %>%
-    dplyr::count(year, month, month_label)
-}
+library(dplyr)
+library(data.table)
+library(lubridate)
+library(anytime)
 
 #* Return the monthly changes of PRs merged
 #* @param json input json payload
-#* @post /monthly_changes
+#* @post /monthly_total_change
 function(json) {
-  payload <- jsonlite::fromJSON(json) %>% as.data.table()
-  payload %>%
+   payload <- jsonlite::fromJSON(json) %>% as.data.table()
+   payload %>% 
     mutate(
       merged_at = lubridate::ymd_hms(merged_at),
-      YearMonth = format(merged_at, '%Y-%m')
+      year_month = format(merged_at, '%Y-%m'),
+      date = anytime::iso8601(anytime(paste0(year_month, '-01')))
     ) %>%
-    filter(!is.na(year_month)) %>%
-    group_by(year_month) %>%
-    summarise(monthly_total = n()) %>%
-    arrange(year_month) %>%
+    filter(!is.na(year_month)) %>% 
+    group_by(date) %>% 
+    summarise(total = n()) %>% 
+    arrange(date) %>% 
     mutate(
-      monthly_change = (monthly_total - lag(monthly_total)) / lag(monthly_total)
+      change = (total - lag(total)) / lag(total),
+      mom_change  = (total - lag(total, 12)) / lag(total,12)
     )
 }
+
+#* Return the weekly changes of PRs merged
+#* @param json input json payload
+#* @post /weekly_total_change
+function(json) {
+  payload <- jsonlite::fromJSON(json) %>% as.data.table()
+  payload %>% 
+    mutate(
+      merged_at = lubridate::ymd_hms(merged_at),
+      year_week = format(merged_at, '%Y-%U'),
+      date = anytime::iso8601(anytime(as.Date(lubridate::floor_date(merged_at, unit = 'week'))))
+    ) %>%
+    filter(!is.na(date)) %>% 
+    group_by(date) %>% 
+    summarise(total = n()) %>% 
+    arrange(date) %>%
+    mutate(
+      change = (total - lag(total))
+    )
+}
+
+#* Return the monthly changes of PRs merged last 30 days compared to previous
+#* @param json input json payload
+#* @post /last30_total
+function(json) {
+  payload <- jsonlite::fromJSON(json) %>% as.data.table()
+  payload %>% 
+    mutate(
+      merged_at = lubridate::ymd_hms(merged_at)) %>%
+    filter(!is.na(year_month) & merged_at > (Sys.Date() - days(60))) %>%
+    mutate(group = factor(case_when(
+      merged_at < Sys.Date() - days(30) ~ 'previous',
+      TRUE ~ 'current'), 
+      ordered = TRUE,
+      levels = c('previous','current'))) %>% 
+    group_by(group) %>% 
+    summarise(total = n()) %>% 
+    arrange(group) %>%  
+    mutate(
+      change = (total - lag(total)) / lag(total)
+    )
+}
+
+
