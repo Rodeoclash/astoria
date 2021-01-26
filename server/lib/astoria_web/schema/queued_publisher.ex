b89ee125b69e4ef46f84c9800ca5784c9a0a8621
Sam Richardson <sam@richardson.co.nz>
2021-01-26 15:55:31 +1100

    Queued publisher for subscriptions

--- server/lib/astoria_web/schema/queued_publisher.ex
+++ server/lib/astoria_web/schema/queued_publisher.ex
@@ -1,7 +1,15 @@
 defmodule AstoriaWeb.Schema.QueuedPublisher do
+  alias __MODULE__
   use GenServer
 
   @interval 250
+
+  @enforce_keys [
+    :args,
+    :publish_at
+  ]
+
+  defstruct args: [], publish_at: nil
 
   def start_link(_default) do
     GenServer.start_link(__MODULE__, %{}, name: :github_repository_update_register)
@@ -24,41 +32,43 @@ defmodule AstoriaWeb.Schema.QueuedPublisher do
 
     Process.send_after(self(), :tick, @interval)
 
-    {:noreply, state}
+    new_state =
+      Enum.reduce(state, %{}, fn {id, publisher}, acc ->
+        case DateTime.compare(publisher.publish_at, DateTime.utc_now()) do
+          n when n in [:lt, :eq] ->
+            apply(Absinthe.Subscription, :publish, publisher.args)
+
+            acc
+
+          :gt ->
+            Map.put(acc, id, publisher)
+        end
+      end)
+
+    {:noreply, new_state}
   end
 
   @impl true
-  def handle_cast({:queue_publish, pubsub, mutation_result, info, options}, state) do
+  def handle_cast({:queue_publish, args, options}, state) do
     id = Keyword.get(options, :id)
     window = Keyword.get(options, :window, 1)
 
-    new_state =
+    publish_at =
       case Map.get(state, id) do
         nil ->
-          Map.merge(state, %{
-            id => %{
-              payload: [
-                info: info,
-                mutation_result: mutation_result,
-                pubsub: pubsub
-              ],
-              publish_at: DateTime.add(DateTime.utc_now(), window)
-            }
-          })
+          DateTime.add(DateTime.utc_now(), window)
 
         existing ->
-          %{
-            state
-            | id => [
-                payload: [
-                  info: info,
-                  mutation_result: mutation_result,
-                  pubsub: pubsub
-                ],
-                publish_at: existing[:publish_at]
-              ]
-          }
+          existing.publish_at
       end
+
+    new_state =
+      Map.merge(state, %{
+        id => %QueuedPublisher{
+          args: args,
+          publish_at: publish_at
+        }
+      })
 
     {:noreply, new_state}
   end
